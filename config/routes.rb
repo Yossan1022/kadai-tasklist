151f90d22616018f66b966febe8d22d99024423f
Ubuntu <ubuntu@ip-172-31-50-80.ec2.internal>
2022-08-27 15:19:49 +0000

    tasklist

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
      root to: 'tasks#index'
+     get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+     get 'signup', to: 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index, :show, :create]
   resources :tasks
+  
 end
