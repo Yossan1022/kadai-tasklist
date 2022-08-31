0b2fd1a4b1bab1f047f0f549cfbc873fa7f357ce
Ubuntu <ubuntu@ip-172-31-50-80.ec2.internal>
2022-08-31 13:09:13 +0000

    d

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,57 +5,49 @@ class TasksController < ApplicationController
  def update
     @task = Task.find(params[:id])
     if @task.update(task_params)
-      flash[:success] = 'Task は正常に更新されました'
-      redirect_to @task
+     flash[:success] = 'Task は正常に更新されました'
+     redirect_to @task
     else
       flash.now[:danger] = 'Task は更新されませんでした'
-      render :edit
+     render :edit
     end
  end
  
  def index
-      @tasks = Task.all
-      
+     @tasks = Task.all
  end
  def edit
-   @task = Task.find(params[:id])
+     @task = Task.find(params[:id])
    
  end
 def show
-  @tasks = Task.all
-   @task = Task.find(params[:id])
-  
+     @task = Task.find(params[:id])
 end
 
 def new
-  user = User.first
-  user.tasks
-  task = user.tasks.build
-  task.save
-   @task = user.tasks.build
+     user = User.last
+     @task = user.tasks.build
 end
 
-  def create
-    @task = Task.new(task_params)
-    if @task.save
-      flash[:success] = 'タスクを投稿しました。'
-      redirect_to root_url
-    else
-      flash.now[:danger] = 'タスクの投稿に失敗しました。'
-      render 'tasks/new'
-    end
+def create
+     user = User.last
+     @task = user.tasks.build(task_params)
+  if @task.save
+     flash[:success] = 'タスクを投稿しました。'
+     redirect_to root_url
+  else
+     flash.now[:danger] = 'タスクの投稿に失敗しました。'
+     render 'tasks/new'
   end
-
+end
   def destroy
      @task = Task.find(params[:id])
-    @task.destroy
-    flash[:success] = 'タスクを削除しました。' 
+     @task.destroy
+     flash[:success] = 'タスクを削除しました。' 
      redirect_to root_url
   end
-
   private
-
   def task_params
-    params.require(:task).permit(:content, :status)
+     params.require(:task).permit(:content, :status)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
      get 'signup', to: 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:index, :show, :create]
+  resources :users, only: [:index,:show,:create]
   resources :tasks
    resources :users
+   
 end
