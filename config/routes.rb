66e2f1a5634b8c30ce9fe05ff2c54a23eff7950e
Ubuntu <ubuntu@ip-172-31-50-80.ec2.internal>
2022-09-02 09:16:36 +0000

    e

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,5 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   get 'signup', to: 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:index,:show,:create]
   resources :tasks
-   
-   
 end
