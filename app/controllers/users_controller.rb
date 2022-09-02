a5ecd702f5f0894ecbcdab4dff03aa89b802d22d
Ubuntu <ubuntu@ip-172-31-50-80.ec2.internal>
2022-09-02 13:21:07 +0000

    q

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,11 @@
 class UsersController < ApplicationController
-   before_action :require_user_logged_in,only: [:index,:show,:edit,:update,:new]
-def require_user_logged_in
-  if @currect_user.id != params[:id].to_i
-    redirect_to root_url
-  end
- end
+  
  def show
    @user = User.find(params[:id])
    redirect_to root_url
  end
  def new
-  @currect_user.id = User.new
+  @user = User.new
  end
  def create
    @user = User.new(user_params)
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :tasks
+
 end
