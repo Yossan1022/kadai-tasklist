75e5c41ef001f9bf93614c1959117d3172c548bf
Ubuntu <ubuntu@ip-172-31-50-80.ec2.internal>
2022-09-03 09:04:50 +0000

    d

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     resources :tasks
-     resources :users, only: [:index]
+     resources :users, only: [:index,:create,:show]
 end
